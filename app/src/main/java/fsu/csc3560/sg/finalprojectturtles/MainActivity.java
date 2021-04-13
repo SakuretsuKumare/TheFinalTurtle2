bbb6bd8eb28a323eeb3aab7288d0674f4a62e4eb
CuriousLink <74274785+CuriousLink@users.noreply.github.com>
2021-04-12 22:17:09 -0400

    Fixed picture issue; Added page connection

--- app/src/main/java/fsu/csc3560/sg/finalprojectturtles/MainActivity.java
+++ app/src/main/java/fsu/csc3560/sg/finalprojectturtles/MainActivity.java
@@ -2,7 +2,10 @@ package fsu.csc3560.sg.finalprojectturtles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void sendSecond(View view) {
+        Intent intent = new Intent(this, SecondPage.class);
+        startActivity(intent);
+    }
+
+    public void sendDonation(View view) {
+        Intent intent = new Intent(this, DonateMoneyActivity.class);
+        startActivity(intent);
+    }
+
+    public void sendHelpSite(View view) {
+        Uri uri = Uri.parse("www.fisheries.noaa.gov/feature-story/what-can-you-do-save-sea-turtles");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
 }
