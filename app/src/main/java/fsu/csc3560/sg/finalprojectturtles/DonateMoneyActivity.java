1ef0e42641c8caf644df4d20756ab0c0482ba3e1
SakuretsuKumare <78335074+SakuretsuKumare@users.noreply.github.com>
2021-04-12 21:17:09 -0400

    Added Project & GitIgnore

---
+++ app/src/main/java/fsu/csc3560/sg/finalprojectturtles/DonateMoneyActivity.java
@@ -0,0 +1,14 @@
+package fsu.csc3560.sg.finalprojectturtles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonateMoneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate_money);
+    }
+}
